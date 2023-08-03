c05cbc3ca62083d7928f3348d75aaeed7d7d3527
LAV-cloud <ZombieManiak172@outlook.com>
2023-08-03 20:40:29 +0600

    Initial Commit

---
+++ ElectroMap/ElectroMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectroMapApp.swift
+//  ElectroMap
+//
+//  Created by Ромка Бережной on 03.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ElectroMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
